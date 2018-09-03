44834547ef9a5504382458926b82e01d2dba0f4f
Yuliya Sinkevich <36645108+YuliyaSinkevich@users.noreply.github.com>
2018-09-03 06:47:17 +0300

    Main page (except switcher for map) (#5)
    * Main page (except switcher for map)
    
    * Delete accelerator.iml
    
    .idea temporary file
    
    * Delete workspace.xml
    
    .idea temporary file
    
    * Delete R_User_Library.xml
    
    .idea temporary file
    
    * Delete misc.xml
    
    .idea temporary file
    
    * Delete modules.xml
    
    .idea temporary file
    
    * Delete vcs.xml
    
    .idea temporary file

--- app/static/assets/js/script.js
+++ app/static/assets/js/script.js
@@ -1,2 +1,8 @@
-﻿
-
+﻿$(document).ready(function () {
+    $("#main-top-navbar").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 500);
+    });
+});

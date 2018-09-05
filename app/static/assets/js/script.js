50862135a727f8c5bb983d231995e766d8fa6d3b
Yuliya Sinkevich <36645108+YuliyaSinkevich@users.noreply.github.com>
2018-09-05 05:49:23 +0300

    TopNav done (#6)

--- app/static/assets/js/script.js
+++ app/static/assets/js/script.js
@@ -5,4 +5,15 @@
             top = $(id).offset().top;
         $('body,html').animate({scrollTop: top}, 500);
     });
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        document.getElementById("main-top-navbar").style.borderTop = "0";
+        } else {
+        document.getElementById("main-top-navbar").style.borderTop = "50px solid #3d4f9b";
+    }
+}
 });

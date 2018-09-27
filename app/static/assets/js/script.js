6275d54f88b33a11fae2b11c906df78907f8e5f2
Yuliya Sinkevich <36645108+YuliyaSinkevich@users.noreply.github.com>
2018-09-27 08:23:43 +0300

    Switch to Yandex Map (#7)
    * TopNav done
    
    * Swith to Yandex Map

--- app/static/assets/js/script.js
+++ app/static/assets/js/script.js
@@ -16,4 +16,29 @@ function scrollFunction() {
         document.getElementById("main-top-navbar").style.borderTop = "50px solid #3d4f9b";
     }
 }
+
+
+var switcherButton = document.getElementById("switcherBut");
+switcherButton.addEventListener("click", toggle, false);
+
+function toggle() {
+var switcher = document.getElementById("switcher");
+var yandexMap = document.getElementById('yandexMap');
+
+    if(switcherButton.style.cssText !== "float: right;") {
+        yandexMap.style.cssText = "display: block;";
+        switcherButton.style.cssText = "float: right;";
+        switcher.style.cssText = "background-color: #cf6f27";
+    } else {
+        yandexMap.style.cssText = "display: none;";
+        switcherButton.style.cssText = "float: left;";
+        switcher.style.cssText = "background-color: #3d4f9b;";
+    }
+}
+
 });
+
+
+
+
+
